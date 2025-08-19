f4723aa12cc95ac5c6dd2b832b3f78a08581ea0e
Ekraj <ekrajkhatri1247@gmail.com>
2025-08-20 00:30:26 +0530

    auth.py

--- collector/auth.py
+++ collector/auth.py
@@ -2,6 +2,8 @@
 from SmartApi import SmartConnect
 from pyotp import TOTP
 
+# Authenticate with SmartAPI and return JWT and feed tokens
+
 def authenticate(api_key: str, client_code: str, password: str, totp_key: str):
     obj = SmartConnect(api_key=api_key)
     session = obj.generateSession(client_code, password, TOTP(totp_key).now())
